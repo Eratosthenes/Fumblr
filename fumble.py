0f1d45667192a2f9c0925e480c8bcb0fd5ace381
Nathan Rapport <nathanrapport@Nathans-MacBook-Air.local>
2018-07-13 18:10:56 -0400

    creating json endpoints

---
+++ fumble.py
@@ -0,0 +1,95 @@
+# usage (in separate terminals): 
+# $gunicorn fumble:app
+# $python3 fumble
+
+import ast
+import random
+import falcon
+import pickle
+import time
+import pdb
+from fumbler import Fumbler
+from multiprocessing import Pool
+from collections import defaultdict
+from cachetools import LRUCache
+
+# location dictionary is stored as an lru cache to prevent memory from overflowing
+loc_d = LRUCache(maxsize=10) # a dictionary with buckets that represent time/loc segments
+match_d = defaultdict(list) # dictionary of matches
+
+# create dictionary of friendships 
+friend_d = defaultdict(set) 
+for rel in eval(open('relationships.json').read()):
+    friend_d[rel['from']].add(rel['to'])
+    friend_d[rel['to']].add(rel['from']) # assume friendship is always mutual
+
+class Resource(object):
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        resp.status = falcon.HTTP_200  
+        user_id = int(req.get_param('userId'))
+        with open('match_d.p', 'rb') as handle:
+            match_d = pickle.load(handle)
+        
+        body = {user_id: match_d[user_id]} if match_d.get(user_id) else {user_id: []}
+        resp.body = '{}'.format(body)
+
+
+    def on_post(self, req, resp):
+        """Handles POST requests"""
+        resp.status = falcon.HTTP_201
+        d = ast.literal_eval(req.stream.read().decode('utf-8')) 
+        ts = time.time()
+        resp.body = 'userId={}; lat={}; long={}; ts={}'.format(d['userId'], d['lat'], d['lon'], ts)
+        print(resp.body)
+        
+        # populate location dictionary
+        h_data = self.granularize(d['lat'], d['lon'], ts) # granularized hashed data
+        self.populate_loc_d(h_data, d['userId'])
+
+        # populate match dictionary 
+        # shouldn't take too long as long as people have a limited number of friends
+        global match_d
+        for user in friend_d[d['userId']]:
+            if user in loc_d[h_data]:
+                match_d[d['userId']].append(user)
+                match_d[user].append(d['userId'])
+
+        # batch writing for the match dictionary (with a very low threshold)
+        # TODO: load/combine with previous match data so that we're not overwriting
+        if len(match_d) > 1:
+            print(match_d)
+            with open('match_d.p', 'wb') as handle:
+                pickle.dump(match_d, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+            match_d.clear() # technically not necessary since it's not referenced elsewhere
+            match_d = defaultdict(list)
+    
+    def populate_loc_d(self, h_data, user_id):
+        if not loc_d.get(h_data):
+            loc_d[h_data] = set([user_id])
+        loc_d[h_data].add(user_id)
+
+    # round these numbers to whatever you like in order to define a match
+    def granularize(self, lat, lon, ts):
+        lat = "{0:.2f}".format(round(lat, 2))
+        lon = "{0:.2f}".format(round(lon, 2))
+        ts = "{0:.0f}".format(round(time.time(), -2))
+        return hash(' '.join([lat, lon, ts]))
+
+app = falcon.API()
+app.add_route('/', Resource())
+
+if __name__=='__main__':
+    # create a matching pair of users
+    users = []
+    users.append(Fumbler(1, 10.01, 20.0))
+    users.append(Fumbler(2, 10.007, 20.003))
+
+    # create some non-matching users
+    for i in range(3,6):
+        users.append(Fumbler(i))
+
+    # simulate roaming
+    p = Pool(len(users))
+    p.map(Fumbler.roam, users)
---
+++ fumbler.py
@@ -0,0 +1,47 @@
+import random
+import time
+import os
+
+class Fumbler():
+    """
+    Simulate Fumble users
+    """
+
+    def __init__(self, userId, lat=None, lon=None):
+        self.userId = userId
+        if lat and lon:
+            self.lat, self.lon = lat, lon
+        else:
+            self.lat, self.lon = self.generate_loc()
+
+    def roam(self):
+        """
+        Randomly roam around
+        """
+        while True:
+            self.publish_loc()
+            self.change_loc()
+            time.sleep(1)
+
+    def get_loc(self):
+        return self.lat, self.lon
+
+    def generate_loc(self):
+        lat = round(random.randint(0, 90) + random.random(), 4)
+        lon = round(random.randint(0, 180) + random.random(), 4)
+        return lat, lon
+
+    def change_loc(self):
+        """
+        Move by up to one degree (within bounds)
+        """
+        self.lat += random.uniform(-1,1)
+        self.lon += random.uniform(-1,1)
+        self.lat = min(max(0, self.lat), 90) 
+        self.lon = min(max(0, self.lat), 180) 
+
+    # in the real world we probably wouldn't send everything all to the same machine/port
+    def publish_loc(self):
+        url = "http://localhost:8000"
+        os.system('http POST \"{}\" userId:={} lon:={} lat:={}'.format(url, self.userId, self.lon, self.lat))
+
